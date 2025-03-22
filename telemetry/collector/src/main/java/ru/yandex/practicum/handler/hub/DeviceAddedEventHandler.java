05c72739914d648c65309872141c57e3b54ac1ba
tutrahamon <tutrachamon@mail.ru>
2025-03-22 08:09:29 +0300

    add handler

--- telemetry/collector/src/main/java/ru/yandex/practicum/handler/hub/DeviceAddedEventHandler.java
+++ telemetry/collector/src/main/java/ru/yandex/practicum/handler/hub/DeviceAddedEventHandler.java
@@ -1,12 +1,24 @@
 package ru.yandex.practicum.handler.hub;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 import ru.yandex.practicum.grpc.telemetry.event.HubEventProto;
+import ru.yandex.practicum.kafka.KafkaAvroProducer;
 
+@Slf4j
+@Component
+@RequiredArgsConstructor
 public class DeviceAddedEventHandler implements HubEventHandler {
+
+    @Value("${hub.topic}")
+    private String topic;
+    private final KafkaAvroProducer producer;
 
     @Override
     public HubEventProto.PayloadCase getMessageType() {
-        return null;
+        return HubEventProto.PayloadCase.DEVICE_ADDED;
     }
 
     @Override
--- telemetry/collector/src/main/java/ru/yandex/practicum/handler/sensor/LightSensorEventHandler.java
+++ telemetry/collector/src/main/java/ru/yandex/practicum/handler/sensor/LightSensorEventHandler.java
@@ -1,12 +1,24 @@
 package ru.yandex.practicum.handler.sensor;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 import ru.yandex.practicum.grpc.telemetry.event.SensorEventProto;
+import ru.yandex.practicum.kafka.KafkaAvroProducer;
 
+@Slf4j
+@Component
+@RequiredArgsConstructor
 public class LightSensorEventHandler implements SensorEventHandler {
+
+    @Value("${sensor.topic}")
+    private String topic;
+    private final KafkaAvroProducer producer;
 
     @Override
     public SensorEventProto.PayloadCase getMessageType() {
-        return null;
+        return SensorEventProto.PayloadCase.LIGHT_SENSOR_EVENT;
     }
 
     @Override
