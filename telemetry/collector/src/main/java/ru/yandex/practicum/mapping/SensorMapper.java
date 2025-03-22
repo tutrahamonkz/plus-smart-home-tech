021d6fd8b1379c23842f5d1b6700ae5b5ad3d721
tutrahamon <tutrachamon@mail.ru>
2025-03-22 08:56:23 +0300

    add handler

--- telemetry/collector/src/main/java/ru/yandex/practicum/mapping/SensorMapper.java
+++ telemetry/collector/src/main/java/ru/yandex/practicum/mapping/SensorMapper.java
@@ -1,11 +1,15 @@
 package ru.yandex.practicum.mapping;
 
+import com.google.protobuf.Timestamp;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.Named;
 import org.mapstruct.factory.Mappers;
 import ru.yandex.practicum.exception.BadRequestException;
+import ru.yandex.practicum.grpc.telemetry.event.*;
 import ru.yandex.practicum.kafka.telemetry.event.*;
+
+import java.time.Instant;
 
 @Mapper
 public interface SensorMapper {
@@ -13,23 +17,27 @@ public interface SensorMapper {
     SensorMapper INSTANCE = Mappers.getMapper(SensorMapper.class);
 
     @Mapping(target = "payload", source = ".", qualifiedByName = "mapPayload")
-    SensorEventAvro mapSensorToAvro(SensorEvent event);
+    SensorEventAvro mapSensorToAvro(SensorEventProto event);
 
     @Named("mapPayload")
-    default Object mapPayload(SensorEvent event) {
-        return switch (event.getType()) {
-            case LIGHT_SENSOR_EVENT -> mapSensorToAvro((LightSensorEvent) event);
-            case MOTION_SENSOR_EVENT -> mapSensorToAvro((MotionSensorEvent) event);
-            case CLIMATE_SENSOR_EVENT -> mapSensorToAvro((ClimateSensorEvent) event);
-            case SWITCH_SENSOR_EVENT -> mapSensorToAvro((SwitchSensorEvent) event);
-            case TEMPERATURE_SENSOR_EVENT -> mapSensorToAvro((TemperatureSensorEvent) event);
-            default -> throw new BadRequestException("Задан неверный тип сенсора " + event.getType());
+    default Object mapPayload(SensorEventProto event) {
+        return switch (event.getPayloadCase()) {
+            case LIGHT_SENSOR_EVENT -> mapSensorToAvro((LightSensorProto) event);
+            case MOTION_SENSOR_EVENT -> mapSensorToAvro((MotionSensorProto) event);
+            case CLIMATE_SENSOR_EVENT -> mapSensorToAvro((ClimateSensorProto) event);
+            case SWITCH_SENSOR_EVENT -> mapSensorToAvro((SwitchSensorProto) event);
+            case TEMPERATURE_SENSOR_EVENT -> mapSensorToAvro((TemperatureSensorProto) event);
+            default -> throw new BadRequestException("Задан неверный тип сенсора " + event.getPayloadCase());
         };
     }
 
-    ClimateSensorAvro mapSensorToAvro(ClimateSensorEvent event);
-    LightSensorAvro mapSensorToAvro(LightSensorEvent event);
-    MotionSensorAvro mapSensorToAvro(MotionSensorEvent event);
-    SwitchSensorAvro mapSensorToAvro(SwitchSensorEvent event);
-    TemperatureSensorAvro mapSensorToAvro(TemperatureSensorEvent event);
+    ClimateSensorAvro mapSensorToAvro(ClimateSensorProto event);
+    LightSensorAvro mapSensorToAvro(LightSensorProto event);
+    MotionSensorAvro mapSensorToAvro(MotionSensorProto event);
+    SwitchSensorAvro mapSensorToAvro(SwitchSensorProto event);
+    TemperatureSensorAvro mapSensorToAvro(TemperatureSensorProto event);
+
+    default Instant map(Timestamp timestamp) {
+        return Instant.ofEpochSecond(timestamp.getSeconds(), timestamp.getNanos());
+    }
 }
