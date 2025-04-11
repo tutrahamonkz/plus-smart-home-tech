1f26063b6eac40b0e181722e07f2dd67847155fb
tutrahamon <tutrachamon@mail.ru>
2025-04-11 16:57:22 +0300

    initialize project, create module

---
+++ commerce/interaction-api/src/main/java/ru/yandex/practicum/model/ProductDto.java
@@ -0,0 +1,12 @@
+package ru.yandex.practicum.model;
+
+public class ProductDto {
+    private String productId;
+    private String productName;
+    private String description;
+    private String imageSrc;
+    private QuantityState quantityState;
+    private ProductState productState;
+    private ProductCategory productCategory;
+    private Double price;
+}
